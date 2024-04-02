b91c7fd110d2655f1bc5259d1341ffcae0cf90f0
Jokerrorr <3321806364@qq.com>
2024-04-02 22:54:37 +0800

    非常好训练，10个epoch使我爆炸

--- train.py
+++ train.py
@@ -9,8 +9,8 @@ from net import net
 from train_model import train_model, save_dir
 from view_acc_and_loss import *
 
-epochs = 50
-batch_size = 128
+epochs = 10
+batch_size = 64
 num_classes = 100
 learning_rate = 1e-2
 filename = save_dir + '/checkpoint.pth'  # 模型保存文件
